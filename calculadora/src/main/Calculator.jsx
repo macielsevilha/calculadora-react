7046eafee62f7b00fd4f9c98196381a4fad5db9c
MacielSevilha <dev.macielsevilha@gmail.com>
2022-06-12 20:58:54 -0300

    sistema #04

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -28,14 +28,58 @@ clearMemory() {
 }
 
 setOperation(operation) {
-  console.log(this.value)
+  
+  if (this.state.current === 0) {
+    this.setState({ operation, current: 1, clearDisplay: true})
+  
+  } else {
+    const equals = operation === '=' //true
+    const currentOperation = this.state.operation
+    
+    const values = [...this.state.values]
+
+    console.log(equals)
+     try {
+      values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+    } catch (error) {
+       values[0] = this.state.values[0]
+    }
+
+    values[1] = 0
+
+   this.setState({
+    displayValue: values[0],
+    operation: equals ? null : operation,
+    current: equals ? 0 : 1,
+    clearDisplay: !equals,
+    values
+  
+   })
+  }
 }
 addDigit(n) {
-  
- this.setState({
-   displayValue: n
- })
 
+  if (n === '.' && this.state.displayValue.includes('.')) {
+    return
+  }
+ 
+  const clearDisplay = this.state.displayValue === '0' 
+       || this.state.clearDisplay
+
+  const currentValue = clearDisplay ? '' : this.state.displayValue
+  const displayValue = currentValue + n
+  this.setState({ displayValue, clearDisplay: false})
+
+  if (n != '.') {
+    const i = this.state.current 
+    const newValue = parseFloat(displayValue)
+  
+    const values = [...this.state.values]
+    values[i] = newValue
+    this.setState( {values} )
+  
+    console.log(this.state.values)
+  }
 }
   render() {
       return(
